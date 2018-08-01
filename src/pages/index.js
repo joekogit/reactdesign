b3125613ced70cf3f3c29b36f2ac3f708ac91b1e
jkocov53 <joseph.kocovsky@avant.com>
2018-08-01 12:11:50 -0500

    revert previous commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Spicy jalapeno bacon ipsum</h1>
-    <p>Pork belly ground round pork chop rump shankle capicola. Picanha turkey cow kielbasa brisket drumstick hamburger short loin pork jerky tail t-bone beef biltong. Pork bresaola rump tongue pork belly kielbasa pork chop tri-tip shoulder alcatra. Ham doner tenderloin jerky.</p>
+    <p>Pork belly ground round pork chop rump shankle capicola. Picanha cow turkey kielbasa brisket drumstick hamburger short loin pork jerky tail t-bone beef biltong. Pork bresaola rump tongue pork belly kielbasa pork chop tri-tip shoulder alcatra. Ham doner tenderloin jerky.</p>
     <p>Pork flank tongue shank. Frankfurter cow beef ribs buffalo t-bone cupim leberkas kevin ham hock tongue beef. Pastrami buffalo porchetta fatback kielbasa boudin brisket, salami prosciutto t-bone biltong shankle.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
